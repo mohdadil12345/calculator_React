ebb800ac83552422155275272d4d98f06498dcd4
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-07 20:46:15 +0530

    css design update

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,41 +1,167 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Calculator() {
-    const [inputval, setinputval] = useState("")
+  const [inputval, setinputval] = useState("");
 
   return (
-    <>
-    
-        <h1 className='heading'>Calculator</h1>
-    <div className='main_container'>
-    <input   onChange={(e) => setinputval(e.target.value)} value={inputval} type="text" placeholder='calculate' />
+    <div className="body">
+      <div className="main_container">
+        <div className="calculator">
+          <input
+            onChange={(e) => setinputval(e.target.value)}
+            value={inputval}
+            type="text"
+            placeholder="0"
+            id="inputBox" 
+          />
 
-  <div className="box">
-     <button value="1" onClick={(e) => setinputval(inputval + e.target.value)}>1</button>
-     <button value="2" onClick={(e) => setinputval(inputval + e.target.value)}>2</button>
-     <button value="3" onClick={(e) => setinputval(inputval + e.target.value)}>3</button>
-     <button value="+" onClick={(e) => setinputval(inputval + e.target.value)}>+</button>
-     <button value="4" onClick={(e) => setinputval(inputval + e.target.value)}>4</button>
-     <button value="5" onClick={(e) => setinputval(inputval + e.target.value)}>5</button>
-     <button value="6" onClick={(e) => setinputval(inputval + e.target.value)}>6</button>
-     <button value="-" onClick={(e) => setinputval(inputval + e.target.value)}>-</button>
-     <button value="7" onClick={(e) => setinputval(inputval + e.target.value)}>-</button>
-     <button value="8" onClick={(e) => setinputval(inputval + e.target.value)}>8</button>
-     <button value="9" onClick={(e) => setinputval(inputval + e.target.value)}>9</button>
-     <button value="*" onClick={(e) => setinputval(inputval + e.target.value)}>*</button>
-     <button value="c" onClick={(e) => setinputval("")}>C</button>
-     <button value="0" onClick={(e) => setinputval( inputval + e.target.value)}>0</button>
-     <button value="00" onClick={(e) => setinputval( inputval + e.target.value)}>00</button>
-     <button value="." onClick={(e) => setinputval( inputval + e.target.value)}>.</button>
-     <button  value="/" onClick={(e) => setinputval(inputval + e.target.value)}>/</button>
-     <button  onClick={()=> setinputval(inputval.slice(0, -1)) } >DEL</button>
-     <button className='equal'  onClick={(e) => setinputval(eval(inputval))}>=</button>
+        
+            <div>
+              <button
+              class="button"
+                value="1"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                1
+              </button>
+              <button
+              class="button"
+                value="2"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                2
+              </button>
+              <button
+                value="3"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                3
+              </button>
+              <button
+                value="+"
+                class="button operator"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                +
+              </button>
+            </div>
 
-  </div>
+            <div>
+              <button
+                value="4"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                4
+              </button>
+              <button
+                value="5"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                5
+              </button>
+              <button
+                value="6"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                6
+              </button>
+              <button
+                value="-"
+                class="button operator"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                -
+              </button>
+            </div>
 
-    </div>
-    </>
-  )
+            <div>
+              <button
+                value="7"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                -
+              </button>
+              <button
+                value="8"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                8
+              </button>
+              <button
+                value="9"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                9
+              </button>
+              <button
+                value="*"
+                class="button operator"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                *
+              </button>
+            </div>
+
+            <div>
+              <button  class="button operator" value="c" onClick={(e) => setinputval("")}>
+                AC
+              </button>
+              <button
+                value="0"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                0
+              </button>
+              <button
+                value="00"
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                00
+              </button>
+              <button
+                value="."
+                class="button"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                .
+              </button>
+            </div>
+
+            <div>
+              <button
+              class="button operator"
+                value="/"
+                onClick={(e) => setinputval(inputval + e.target.value)}
+              >
+                /
+              </button>
+              <button class="button operator"  onClick={() => setinputval(inputval.slice(0, -1))}>
+                DEL
+              </button>
+              <button
+                className="equalBtn"
+                onClick={(e) => setinputval(eval(inputval))}
+              >
+                =
+              </button>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
+
+  );
 }
 
-export default Calculator
+export default Calculator;
